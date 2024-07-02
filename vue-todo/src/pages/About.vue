acacf611e8684934f4cbd3ba715bb74229463d1e
carlow7 <carlesonkalebe2016@gmail.com>
2024-07-01 20:40:29 -0400

    [ADD] Scss

---
+++ vue-todo/src/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Agora vai</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
